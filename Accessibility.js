19562d749d6cc8577d9ab699c0a94b7aeeae74cc
ShalevSha <144739335+ShalevSha@users.noreply.github.com>
2026-04-26 22:24:00 +0300

    Add Accessibility Widget script to Accessibility.js

---
+++ Accessibility.js
@@ -0,0 +1,12 @@
+<script>
+	// Accessibility Widget
+  (function(d){
+    var s = d.createElement("script");
+    s.setAttribute("data-color", "#111111");
+    s.setAttribute("data-position", 5);
+    s.setAttribute("data-size", "small");
+    s.setAttribute("data-account", "PoJJ4CiM9z");
+    s.setAttribute("src", "https://cdn.userway.org/widget.js");
+    (d.body || d.head).appendChild(s);})(document)
+</script>
+
